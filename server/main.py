9158be8454a4acb0988816e8859c2f5e8ec906df
Likithkg <109513581+Likithkg@users.noreply.github.com>
2026-05-18 09:56:48 +0000

    feat: added feature to use custom Agent. Now thw Agent info is stored in db rather then in .env file.

--- server/main.py
+++ server/main.py
@@ -12,6 +12,8 @@ from auth.authenticate import get_current_user
 from models.user import UserModel, CreateUserModel
 from models.application import ApplicationModel, CreateApplicationModel, UpdateApplicationModel
 from models.flows import FlowModel, CreateFlowModel, UpdateFlowModel
+from models.CustomeAgent import CustomAgent, CreateCustomAgent, UpdateCustomAgent
+from helper import encoder
 from config.config import AUTHORIZED_ORIGINS
 from database.database import db, init_db
 from tools import agents
@@ -349,3 +351,70 @@ def agent(data: dict):
     reasoning = data.get("reasoning", False)
 
     return agents.agent(prompt, name, temp, input, reasoning)
+
+@app.post("/tools/CustomAgent")
+async def Create_CustomeAgent(data: CreateCustomAgent,
+ current_user = Depends(get_current_user)
+ ):
+    encoded_api_key = encoder.encrypt(data.api_key)
+    current_time = datetime.now(timezone.utc)
+    new_agent = {
+        "name": data.name,
+        "api_key": encoded_api_key,
+        "endpoint": data.endpoint,
+        "user_id": current_user["clerk_id"],
+        "created_at":current_time,
+        "updated_at": None
+    }
+    await db.CustomAgent.insert_one(new_agent)
+    return {
+        "status": "success",
+        "message": "Application created successfully",
+    }
+
+@app.get("/tools/CustomAgent")
+async def get_CustomAgent(
+    
+    current_user: Depends(get_current_user)
+):
+    agents = await db.custom_agents.find(
+        {"user_id": current_user["clerk_id"]}
+    ).to_list(100)
+
+    for agent in agents:
+        agent["_id"] = str(agent["_id"])
+        agent.pop("api_key", None)
+    return {"status":"success","agents": agents}
+
+@app.update("/tools/CustomAgent/{_id}")
+async def update_CustomAgent(
+    data: UpdateCustomAgent,
+    _id: str
+):
+    
+    CustomAgent = await db.CustomAgent.find_one({"_id": _id})
+    if not CustomAgent:
+        raise HTTPException(status_code=404, detail="Agent not found")
+
+    update_data = {k: v for k, v in data.dict().items() if v is not None}
+
+    if "api_key" in update_data :
+        update_data["api_key"] = encoder.encrypt(update_data["api_key"])
+    
+    update_data["updated_at"] = datetime.now(timezone.utc)
+
+    await db.CustomAgent.update_one(
+        {"_id": ObjectId(_id)},
+        {"$set": update_data}
+    )
+    return {"status": "success", "message": "Agent updated successfully"}
+
+@app.delete("/tools/CustomAgent/{_id}")
+async def Delete_CustomAgent(
+    _id: str
+):
+    CustomAgent = await db.CustomAgent.find_one({"_id": _id})
+    if not CustomAgent:
+        raise HTTPException(status_code=404, detail="Agent not found")
+    await db.CustomAgent.delete_one({"_id": ObjectId(_id)})       
+    return {"status":"success","message": "Agent deleted successfully"}    
